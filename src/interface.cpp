ba5bde404023c9a54d77aa696d255e0c3921fcee
Aiden <aiden.pachel@isd31.net>
2025-11-22 20:34:00 -0600

    Assign "testmove" to testmove_button_action

--- src/interface.cpp
+++ src/interface.cpp
@@ -424,7 +424,7 @@ void create_test_tab(lv_obj_t * parent_tab) {
     lv_obj_t * testmove = lv_btn_create(cont);
     lv_obj_set_pos(testmove, 10, 10);
     lv_obj_set_size(testmove, 150, 40);
-    lv_obj_add_event_cb(testmove, NULL, LV_EVENT_CLICKED, NULL);
+    lv_obj_add_event_cb(testmove, testmove_button_action, LV_EVENT_CLICKED, NULL);
     lv_obj_add_style(testmove, &style_m3_btn, 0);
     lv_obj_set_style_bg_color(testmove, M3_ACCENT_COLOR, LV_PART_MAIN | LV_STATE_PRESSED | LV_STATE_CHECKED);
     lv_obj_set_style_shadow_width(testmove, 0, LV_PART_MAIN | LV_STATE_PRESSED | LV_STATE_CHECKED);
