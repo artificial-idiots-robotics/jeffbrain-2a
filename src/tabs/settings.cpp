a1c9af199c43c457ab0ebb8fbfcb5e13440d9274
Aiden <aiden99pachel@gmail.com>
2025-12-18 19:06:53 -0600

    rename control mode dropdown

--- src/tabs/settings.cpp
+++ src/tabs/settings.cpp
@@ -12,17 +12,17 @@ static void drive_mode_dropdown_action(lv_event_t * e) {
 void create_settings_tab(lv_obj_t * parent_tab) {
     lv_obj_t * cont = create_tab_content_container(parent_tab, LV_FLEX_FLOW_COLUMN);
 
-    lv_obj_t * config_dropdown = lv_dropdown_create(cont);
-    lv_obj_set_pos(config_dropdown, 50, 10);
-    lv_dropdown_set_options(config_dropdown, "Arcade\nTank");
-    lv_obj_add_event_cb(config_dropdown, drive_mode_dropdown_action, LV_EVENT_VALUE_CHANGED, NULL);
+    lv_obj_t * control_mode_dropdown = lv_dropdown_create(cont);
+    lv_obj_set_pos(control_mode_dropdown, 50, 10);
+    lv_dropdown_set_options(control_mode_dropdown, "Arcade\nTank");
+    lv_obj_add_event_cb(control_mode_dropdown, drive_mode_dropdown_action, LV_EVENT_VALUE_CHANGED, NULL);
 
-    lv_obj_set_style_radius(config_dropdown, 15, LV_PART_MAIN);
-    lv_obj_set_style_bg_color(config_dropdown, lv_color_hex(0x49454F), LV_PART_MAIN);    
-    lv_obj_set_style_bg_color(config_dropdown, M3_ACCENT_COLOR, LV_PART_MAIN | LV_STATE_PRESSED);
-    lv_obj_set_style_text_color(config_dropdown, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_style_radius(control_mode_dropdown, 15, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(control_mode_dropdown, lv_color_hex(0x49454F), LV_PART_MAIN);    
+    lv_obj_set_style_bg_color(control_mode_dropdown, M3_ACCENT_COLOR, LV_PART_MAIN | LV_STATE_PRESSED);
+    lv_obj_set_style_text_color(control_mode_dropdown, lv_color_white(), LV_PART_MAIN);
 
-    lv_obj_t * list = lv_dropdown_get_list(config_dropdown);
+    lv_obj_t * list = lv_dropdown_get_list(control_mode_dropdown);
 
     if (list != NULL) {
         lv_obj_set_style_bg_color(list, lv_color_hex(0x49454F), LV_PART_MAIN);
