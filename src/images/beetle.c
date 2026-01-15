d28b74110291db3ab0f4946b08574f50adab78ab
Aiden <artificial.idiots.team@gmail.com>
2026-01-15 19:13:47 +0000

    Revert "fix image include calls"
    This reverts commit 58380497c54a301b1f51f5f85379e15eaa7acb3b.

--- src/images/beetle.c
+++ src/images/beetle.c
@@ -6,7 +6,11 @@
     #endif
 #endif
 
-#include "liblvgl/lvgl.h"
+#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
+    #include "lvgl.h"
+#else
+    #include "lvgl/lvgl.h"
+#endif
 
 
 #ifndef LV_ATTRIBUTE_MEM_ALIGN
